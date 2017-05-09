2c843655e3af996191adedb85c82cebbb609421a
Ian Schoonover <iantskon@gmail.com>
2017-05-08 22:13:16 -0500

    Testing visual timer with sessionStorage

--- public/javascripts/timer.js
+++ public/javascripts/timer.js
@@ -3,32 +3,34 @@ if($('#flash')) {
 	$('#flash').delay(5000).fadeOut('slow');
 }
 
-// // visual timer code
-// var timer;
-
-// function startTimer() {
-//   var presentTime = document.getElementById('timer').innerHTML;
-//   var timeArray = presentTime.split(/[:]+/);
-//   var m = timeArray[0];
-//   var s = checkSecond((timeArray[1] - 1));
-//   if(s==59){m=m-1}
-//   $('#timer').html(m + ":" + s);
-//   timer =	setTimeout(startTimer, 1000);
-// }
-
-// function stopTimer() {
-// 	clearTimeout(timer);
-// }
-
-// function checkSecond(sec) {
-//   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
-//   if (sec < 0) {sec = "59"};
-//   return sec;
-// }
-// // -- end visual timer code
-
 // if there are any items in the cart
 if($('#cart-qty').text() > 0) {
+	//// TESTING sessionStorage Timer
+	const start = sessionStorage.start || Date.now();
+	const timespan = 10000;
+	const delay = 1000;
+	
+
+	function countdown () {
+	  const now = Date.now();
+	  // show visual timer
+	  $('#timer-count-down').show('slow');
+
+	  // begin visual timer
+	  $('#timer').html((Math.floor((now - start - timespan) / 1000) * -1));
+
+	  if (now - start < timespan) {
+	    setTimeout(countdown, delay);
+	  } else {
+	    delete sessionStorage.start;
+	  }
+	}
+
+	if (!sessionStorage.start) {
+	  sessionStorage.start = start;
+	}
+
+	countdown();
 	// // show visual timer
 	// $('#timer-count-down').show('slow');
 	// // begin visual timer
@@ -53,3 +55,27 @@ if($('#cart-qty').text() > 0) {
 		// $('#timer-count-down').hide('slow');
 	// }, 10000);
 }
+
+// visual timer code
+// var timer;
+
+// function startTimer() {
+//   var presentTime = document.getElementById('timer').innerHTML;
+//   var timeArray = presentTime.split(/[:]+/);
+//   var m = timeArray[0];
+//   var s = checkSecond((timeArray[1] - 1));
+//   if(s==59){m=m-1}
+//   $('#timer').html(m + ":" + s);
+//   timer =	setTimeout(startTimer, 1000);
+// }
+
+// function stopTimer() {
+// 	clearTimeout(timer);
+// }
+
+// function checkSecond(sec) {
+//   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+//   if (sec < 0) {sec = "59"};
+//   return sec;
+// }
+// -- end visual timer code
